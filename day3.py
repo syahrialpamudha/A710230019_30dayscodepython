0ace223188da1ab957d951e6cf7d381c809b92dc
syahrialpamudha <syahrialpamudha@gmail.com>
2024-06-17 14:21:19 +0700

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,14 @@
+# Mendefinisikan fungsi untuk menampilkan data diri
+def tampilkan_data_diri(nama, kelas, angkatan):
+    print("Data Diri:")
+    print(f"Nama    : {nama}")
+    print(f"Kelas   : {kelas}")
+    print(f"Angkatan: {angkatan}")
+
+# Data diri
+nama = "syahrial"
+kelas = "PTI A"
+angkatan = 2023
+
+# Memanggil fungsi untuk menampilkan data diri
+tampilkan_data_diri(nama, kelas, angkatan)
