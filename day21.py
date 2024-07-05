a332eb378b52d7ec5f3426e8eb1449263edd1f68
syahrialpamudha <syahrialpamudha@gmail.com>
2024-07-05 15:34:22 +0700

    Create day21.py

---
+++ day21.py
@@ -0,0 +1,15 @@
+# Lirik lagu disimpan dalam variabel
+lirik_lagu = """
+Ku menangis, membayangkan
+Betapa kejamnya dirimu atas diriku
+Kau duakan cinta ini
+Kau pergi bersamanya
+"""
+
+# Fungsi untuk menampilkan lirik lagu
+def tampilkan_lirik(lirik):
+    print("Lirik Lagu:")
+    print(lirik)
+
+# Contoh penggunaan
+tampilkan_lirik(lirik_lagu)
